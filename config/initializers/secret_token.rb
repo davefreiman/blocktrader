6faa9b94cabc1a1e9f024d4d1eaffa6e239f08c1
dfry22 <davidfreiman@gmail.com>
2013-10-05 10:38:18 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocktrader::Application.config.secret_key_base = '608a98259577ee7e8ac552378d11f240abbffbfb50723a3b6a86c38d07daaf1500f23af1791a48562777116dc1113ed8911c2d8be9342383611c785461e28b6b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blocktrader::Application.config.session_store :cookie_store, key: '_blocktrader_session'
