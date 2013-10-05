514d0b782da1cab990ae4f5f73a5533f7bec1444
dfry22 <davidfreiman@gmail.com>
2013-10-05 10:45:31 -0400

    adding capybara and factory girl

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Blocktrader
     
     config.generators do |g|
       g.test_framework :rspec, :fixture => false
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"  
     end
   end
 end
