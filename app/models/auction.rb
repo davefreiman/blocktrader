dd1abf21ab12d6968d4522ca36c7080f8e890ca5
dfry22 <davidfreiman@gmail.com>
2013-10-07 14:34:32 -0400

    generating bids model, migrating

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,5 +1,7 @@
 class Auction < ActiveRecord::Base
 	belongs_to :user
+	has_many :bids
+	has_many :users, :through => :bids
 
 	validates :name, presence: true
 	validates :duration, presence: true,
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   has_many :auctions
 
+	has_many :bids
+	has_many :auctions, :through => :bids
+
   validates :email, :presence => true,
 									  :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i},
 										:uniqueness => true
