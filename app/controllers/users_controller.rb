8fcb70e8a572fe1c3360ecd8105d57aba27a7b27
dfry22 <davidfreiman@gmail.com>
2013-10-07 10:40:55 -0400

    generating users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user.new(user_params)
+
+		if @user.save
+			redirect_to users_path
+		else
+			render :new
+		end
+	end
+
+	def index
+		@users = User.all
+	end
+
+	def user_params
+		params.require(:user).permit(:username, :email, :first_name, :last_name, :password)
+	end
+end
