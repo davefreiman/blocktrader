810b60924df936a333318c688b9a16c3872b41a8
Mitchell Armstrong <mitchellarmstrong@gmail.com>
2013-10-09 22:14:29 -0400

    messing with jquery

---
+++ vendor/assets/javascripts/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+   $('.icon').mouseenter(function() {
+       $(this).animate({
+           height: '+=10px'
+       });
+   });
+   $('.icon').mouseleave(function() {
+       $(this).animate({
+           height: '-=10px'
+       }); 
+   });
+});
